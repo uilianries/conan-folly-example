61357029a4f3b252c50ef674b0e5d99df273800e
Uilian Ries <uilianries@gmail.com>
2018-11-21 10:21:10 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,21 @@
 #include <folly/Format.h>
 #include <folly/futures/Future.h>
 #include <folly/executors/ThreadedExecutor.h>
+#include <folly/Uri.h>
 #include <folly/FBString.h>
 
-static void print(const folly::fbstring& value) {
-    const auto formatted = folly::format("Callback Future: {}", value);
-    std::cout << formatted << std::endl;
+static void print_uri(const folly::fbstring& value) {
+    const folly::Uri uri(value);
+    const auto authority = folly::format("The authority from {} is {}", uri.fbstr(), uri.authority());
+    std::cout << authority << std::endl;
 }
 
 int main() {
     folly::ThreadedExecutor executor;
     folly::Promise<folly::fbstring> promise;
     folly::Future<folly::fbstring> future = promise.getSemiFuture().via(&executor);
-    folly::Future<folly::Unit> unit = std::move(future).thenValue(print);
-    promise.setValue("Hello World!");
+    folly::Future<folly::Unit> unit = std::move(future).thenValue(print_uri);
+    promise.setValue("https://conan.io/");
     std::move(unit).get();
     return EXIT_SUCCESS;
 }
